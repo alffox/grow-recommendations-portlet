9fc8c5eb0ec93f1f6a613a23d9d5247dce1605bc
alffox <alfonso.crisci.it@gmail.com>
2019-06-11 12:38:13 +0200

    GROW-831 Update card HEX codes

--- src/main/resources/META-INF/resources/lib/index.es.js
+++ src/main/resources/META-INF/resources/lib/index.es.js
@@ -28,7 +28,7 @@ const mockupData = {
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
             "tags": ["badge", "gamification", "respect", "test1", "test2"],            
             "readCount": "626",
-            "articleCategory": "Share",
+            "articleCategory": "Learn",
 			id: "card-001",
 			star: true,
 			like: true
